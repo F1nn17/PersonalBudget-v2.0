24fd3b55dfced818a4e28ba10e834ef6d4f319b4
Suxov <Suxov@SHIRAKU>
2023-10-19 08:21:22 +0400

    #Create class Income

---
+++ Income.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudget_2._0
+{
+    internal class Income
+    {
+        private int _id;
+        private string _name;
+        private int _money;
+        private string _data;
+
+        public Income() { }
+        public Income(int id, string name, int money, string data)
+        {
+            _id = id;
+            _name = name;
+            _money = money;
+            _data = data;
+        }
+    }
+}
